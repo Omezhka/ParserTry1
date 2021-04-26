5301e53fbd60acc432425a3ff5a351ddbaffacec
Наталья Аверкова <falabellla-61@mail.ru>
2021-04-26 17:38:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+using Microsoft.Office.Interop.Word;
+using System.Diagnostics;
+
+namespace ParserTry1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Stopwatch stopwatch = new Stopwatch();
+            //string Document1 = "start";
+
+            List<string> Izveshenie = new List<string>();
+            string path = @"C:\Users\Наталья\source\repos\ParserTry1\documents\";
+            //string filename = @"C:\Users\Наталья\source\repos\ParserTry1\documents\1.doc";          
+            string filename = "1.doc";
+
+            Application app = new Application();
+            app.Visible = false;
+
+            //Convert(filename, path);
+            //Console.WriteLine("Done");
+
+           
+            //Microsoft.Office.Interop.Word.Document doc = app.Documents.OpenNoRepairDialog(path+filename);
+            stopwatch.Start();
+            Microsoft.Office.Interop.Word.Document doc = app.Documents.OpenNoRepairDialog(path + filename);
+            stopwatch.Stop();
+            Console.WriteLine("Open doc: " + stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine(doc.Paragraphs.Count);
+            try
+            {
+                for (int i = 1; i < doc.Paragraphs.Count; i++ )
+                {
+                    stopwatch.Start();
+                    Izveshenie.Add("\r\n" + doc.Paragraphs[i + 1].Range.Text);
+                   
+                    stopwatch.Stop();
+                    
+                    Console.WriteLine("List add: " + stopwatch.ElapsedMilliseconds);
+                }
+
+            doc.Close();
+
+                foreach (string s in Izveshenie)
+                {
+                    Console.WriteLine(s);
+                }
+               // Console.WriteLine(text);
+                Console.WriteLine("Done");
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //LoadXceed();
+            Console.ReadLine();
+            
+            
+            app.Quit();
+
+        }
+        //public static void Convert(string filename, string path)
+        //{
+        //    Application word = new Application();
+
+        //    string fullpath = (path + filename);
+
+        //    var sourceFile = new FileInfo(fullpath);
+        //    Microsoft.Office.Interop.Word.Document document = word.Documents.OpenNoRepairDialog(sourceFile.FullName);
+        //    string newFileName = sourceFile.FullName.Replace(".doc", ".docx");
+        //    //string newFileName = $"{path}" + "new.docx";
+        //    document.SaveAs2(newFileName, WdSaveFormat.wdFormatXMLDocument, CompatibilityMode: WdCompatibilityMode.wdWord2010);
+        //    //document.Convert();
+        //    word.ActiveDocument.Close();
+        //    word.Quit();
+
+        //}
+        //public static void Convert2XML(string filename, string path)
+        //{
+        //    Application word = new Application();
+
+        //    string fullpath = (path + filename);
+
+        //    var sourceFile = new FileInfo(fullpath);
+        //    Microsoft.Office.Interop.Word.Document document = word.Documents.OpenNoRepairDialog(sourceFile.FullName);
+        //    string newFileName = sourceFile.FullName.Replace(".doc", ".xml");
+        //    //string newFileName = $"{path}" + "new.docx";
+        //    document.SaveAs2(sourceFile, WdSaveFormat.wdFormatXML);
+        //    //document.Convert();
+        //    word.ActiveDocument.Close();
+        //    word.Quit();
+
+        //}
+        //public void LoadXceed()
+        //{
+        //    Xceed.Document.NET.Document doc = Xceed.Words.NET.DocX.Load("H:\\Винда март 2021\\repos\\Диплом\\new.docx");
+        //    string txt = doc.Text;
+        //    DateTime.Parse(txt);
+        //}
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserTry1
+{
+    class Teacher
+    {
+        // доц.Богданова О.Ю. Финансов как это делить?
+        string fullname;
+        string cathedra;
+
+        List<Schedule> ScheduleList; // список с расписанием? как делить четную/нечетную недели?
+
+
+
+    }
+}
